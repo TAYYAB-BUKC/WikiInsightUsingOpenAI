45b738d0dd788f972f204a7b40463f6ac05487a5
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-04-08 23:47:23 +0500

    Landmarks Added in SourceData.

---
+++ WikiInsightUsingOpenAI/SourceData.cs
@@ -0,0 +1,19 @@
+namespace WikiInsightUsingOpenAI;
+
+public class SourceData
+{
+    public static readonly string[] LandmarkNames =
+    [
+        "Eiffel Tower",
+        "Great Wall of China",
+        "Tower Bridge",
+        "Grand Canyon" ,
+        "Red Square" ,
+        "Leaning Tower of Pisa",
+        "Christ the Redeemer (statue)" ,
+        "Sydney Opera House" ,
+        "Statue of Liberty",
+        "Golden Gate Bridge",
+        "Burj Khalifa",
+    ];
+}
