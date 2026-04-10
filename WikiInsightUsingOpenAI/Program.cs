77f39d3290f3e0f0494206b13dfb89566ea616da
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-04-11 02:57:25 +0500

    App Setup Added.

--- WikiInsightUsingOpenAI/Program.cs
+++ WikiInsightUsingOpenAI/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using WikiInsightUsingOpenAI;
+using WikiInsightUsingOpenAI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+Startup.ConfigureServices(builder);
+var app = builder.Build();
+
+var indexingService = app.Services.GetRequiredService<IndexingService>();
+await indexingService.BuildArticleIndex(SourceData.LandmarkNames);
