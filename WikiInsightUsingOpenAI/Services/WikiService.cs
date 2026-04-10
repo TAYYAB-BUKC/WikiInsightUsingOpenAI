bfef2891ddea94b96ee04835ca833b00bfcbb669
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-04-11 01:19:02 +0500

    GetWikipediaPage method implemented in WikiService.

--- WikiInsightUsingOpenAI/Services/WikiService.cs
+++ WikiInsightUsingOpenAI/Services/WikiService.cs
@@ -1,11 +1,19 @@
 using System.Net;
+using System.Text.Json;
+using WikiInsightUsingOpenAI.Models;
 
 namespace WikiInsightUsingOpenAI.Services;
 
 public class WikiService
 {
     private readonly HttpClient client;
-    private readonly string API_BASE_URL = "https://en.wikipedia.org/w/api.php");
+    private readonly string API_BASE_URL = "https://en.wikipedia.org/w/api.php";
+    private readonly string WIKI_PAGEURL = "https://en.wikipedia.org/wiki/";
+    
+    private JsonSerializerOptions? jsonOptions = new JsonSerializerOptions()
+    {
+        PropertyNameCaseInsensitive = true
+    };
 
     public WikiService(IHttpClientFactory factory)
     {
@@ -33,5 +41,35 @@ public class WikiService
         urlBuilder.Query = string.Join("&", queryString.Select(kv => $"{WebUtility.UrlEncode(kv.Key)}={WebUtility.UrlEncode(kv.Value)}"));
 
         return urlBuilder.ToString();
+    }
+
+    async Task<Article> GetWikipediaPage(string url)
+    {
+        using var request = new HttpRequestMessage(HttpMethod.Get, url);
+        using var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+        response.EnsureSuccessStatusCode();
+
+        var json = await response.Content.ReadAsStringAsync();
+        var apiResponse = JsonSerializer.Deserialize<WikiApiResponse>(json, jsonOptions)
+                            ?? throw new InvalidOperationException("Failed to deserialize Wikipedia response");
+
+        var firstPage = apiResponse.Query?.Pages?.FirstOrDefault();
+        if (firstPage is null || firstPage.Missing is true)
+            throw new Exception($"Cou1d not find a Wikipedia page for {url}");
+
+        if (string.IsNullOrWhiteSpace(firstPage.Title) || string.IsNullOrWhiteSpace(firstPage.Extract))
+            throw new Exception($"Empty Wikipedia page returned for {url}");
+
+        var title = firstPage.Title!;
+        var content = firstPage.Extract!.Trim();
+        var id = UtilsService.ToUrlSafeId(title);
+        var pageUrl = $"{WIKI_PAGEURL}{Uri.EscapeDataString(title.Replace(' ', '_'))}";
+
+        return new Article(
+            Id: id,
+            Title: title,
+            Content: content,
+            PageUrl: pageUrl
+        );
     }
 }
