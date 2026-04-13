8960b80f4899759748482a24f26e9c890b81c870
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-04-13 10:53:40 +0500

    Search Endpoint Implemented.

--- WikiInsightUsingOpenAI/Program.cs
+++ WikiInsightUsingOpenAI/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using WikiInsightUsingOpenAI;
 using WikiInsightUsingOpenAI.Services;
 
+var top = 3;
 var builder = WebApplication.CreateBuilder(args);
 Startup.ConfigureServices(builder);
 var app = builder.Build();
@@ -11,4 +13,12 @@ var app = builder.Build();
 // await indexingService.BuildArticleIndex(SourceData.LandmarkNames);
 
 app.UseCors("FrontendCors");
+
+// GET / api/search?query=...
+app.MapGet("/api/search", async (string query, VectorSearchService vectorSearchService) =>
+{
+    var results = await vectorSearchService.FindTopKArticles(query, top);
+    return Results.Ok(results);
+});
+
 app.Run();
