d06ab5ef7eacf55743a8520cbd4b49cc3204cb46
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2026-04-16 23:57:19 +0500

    BuildFullArticleIndex method implemented in IndexingService.

--- WikiInsightUsingOpenAI/Services/IndexingService.cs
+++ WikiInsightUsingOpenAI/Services/IndexingService.cs
@@ -1,4 +1,6 @@
+using System.Collections.Immutable;
 using Pinecone;
+using WikiInsightUsingOpenAI.Models;
 
 namespace WikiInsightUsingOpenAI.Services;
 
@@ -8,13 +10,17 @@ public class IndexingService
     private readonly WikiService wikiService;
     private readonly EmbeddingService embeddingService;
     private readonly ArticleStoreService articleStoreService;
+    private readonly ArticleSplitterService articleSplitterService;
+    private readonly ArticleChunkStoreService articleChunkStoreService;
 
-    public IndexingService(IndexClient indexClient, WikiService wikiService, EmbeddingService embeddingService, ArticleStoreService articleStoreService)
+    public IndexingService(IndexClient indexClient, WikiService wikiService, EmbeddingService embeddingService, ArticleStoreService articleStoreService, ArticleSplitterService articleSplitterService, ArticleChunkStoreService articleChunkStoreService)
     {
         this.indexClient = indexClient;
         this.wikiService = wikiService;
         this.embeddingService = embeddingService;
         this.articleStoreService = articleStoreService;
+        this.articleSplitterService = articleSplitterService;
+        this.articleChunkStoreService = articleChunkStoreService;
     }
 
     public async Task BuildArticleIndex(string[] pageTitles)
@@ -40,6 +46,48 @@ public class IndexingService
             });
 
             articleStoreService.SaveArticle(wikiPage);
+        }
+    }
+
+    public async Task BuildFullArticleIndex(string[] pageTitles)
+    {
+        foreach (var landmark in pageTitles)
+        {
+            var wikiPage = await wikiService.GetWikipediaPageForTitle(landmark, true);
+
+            var sections = wikiService.SplitIntoSections(wikiPage.Content);
+
+            var chunks = sections.SelectMany(section =>
+                            articleSplitterService
+                            .Chunks(wikiPage.Title, section.Content, wikiPage.PageUrl, section.Title))
+                            .Take(25)
+                            .ToImmutableList();
+
+            List<Vector> pineconeVectors = new();
+            foreach (ArticleChunk chunk in chunks)
+            {
+                var stringsToEmbed = $"{chunk.Title} > {chunk.Section}\n\n{chunk.Content}";
+                var vectors = await embeddingService.GenerateEmbeddings([stringsToEmbed]);
+                var pineconeVector = new Vector
+                {
+                    Id = chunk.Id,
+                    Values = vectors[0].Vector,
+                    Metadata = new Metadata
+                    {
+                        {"title", chunk.Title},
+                        {"section", chunk.Section},
+                        {"chunk_index", chunk.ChunkIndex},
+                    }
+                };
+
+                pineconeVectors.Add(pineconeVector);
+                articleChunkStoreService.SaveArticleChunk(chunk);
+            }
+
+            await indexClient.UpsertAsync(new UpsertRequest
+            {
+                Vectors = pineconeVectors
+            });
         }
     }
 }
